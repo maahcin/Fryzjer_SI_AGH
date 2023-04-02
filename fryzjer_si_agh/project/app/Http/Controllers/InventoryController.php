621ab3e1a15f7865e934b6b2a2a545319c13a076
maahcin <49162059+maahcin@users.noreply.github.com>
2023-04-02 18:03:50 +0200

    FRYZ-16 podglad stanu magazynu w wybranym czasie

--- fryzjer_si_agh/project/app/Http/Controllers/InventoryController.php
+++ fryzjer_si_agh/project/app/Http/Controllers/InventoryController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InventoryController extends Controller
 {
-    //
+    //select p.id, p.name, sum(i.quantity) from products p left join inventories i on p.id=i.product_id group by p.id, p.name;
+    public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        /*
+        $deliveries = DB::select(DB::raw("
+        SELECT * FROM deliveries AS d
+            LEFT JOIN delivery__products AS dp ON d.id = dp.id_delivery
+            LEFT JOIN products AS p ON dp.id_product = p.id
+            WHERE d.id = $id
+        "));
+        return view('delivery.show')->with('deliveries', $deliveries);
+        //$deliveries=Delivery::all();
+        return view('inventory.index');
+            //->with('deliveries', $deliveries);
+        */
+        $inventory = DB::select(DB::raw("select p.id, p.name, sum(i.quantity) as sum from products p left join inventories i on p.id=i.product_id group by p.id, p.name;"));
+        return view('inventory.index')->with('inventory', $inventory);
+    }
 }
---
+++ fryzjer_si_agh/project/resources/views/inventory/index.blade.php
@@ -0,0 +1,51 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('All products in inventory') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                <table class="min-w-full divide-y divide-gray-200 mt-4">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Product ID
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Product Name
+                        </th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Quantity
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach($inventory as $i)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-500">#{{ $i->id }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">{{ $i->name }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    @if(!($i->sum))
+                                        <div class="text-sm text-gray-900">0</div>
+                                    @else
+                                        <div class="text-sm text-gray-900">{{ $i->sum }}</div>
+                                    @endif
+                                </td>
+                            </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
